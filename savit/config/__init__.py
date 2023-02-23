dd1a73a67d786b26e0b3891ef4a70b0b48b38fb9
ivansantiagojr <ivansantiago.junior@gmail.com>
2023-02-23 06:41:12 -0300

    refactor: split function to get shell history string

--- savit/config/__init__.py
+++ savit/config/__init__.py
@@ -10,58 +10,39 @@ DOT_CONFIG_FILE = Path(typer.get_app_dir("savit"))
 CONFIG_FILE: Path = Path(DOT_CONFIG_FILE) / "config.toml"
 
 
-def set_env():
+def _get_shell_history(home: Path, config_file_name: str) -> str:
+    return str(home) + config_file_name
+
+
+def set_env() -> None:
     DOT_CONFIG_FILE.mkdir(parents=True, exist_ok=True)
     CONFIG_FILE.touch(exist_ok=True)
 
-    shell = os.getenv("SHELL")
+    shell = str(os.getenv("SHELL"))
     home = Path.home()
-    if shell.find("zsh") != -1:
-        config_toml_string = f"""[savit]
-history_path = "{home}/.zhistory"
-"""
-        with CONFIG_FILE.open("w") as f:
-            f.write(config_toml_string)
-
-        with CONFIG_FILE.open("rb") as f:
-            config_toml = tomllib.load(f)
-        HISTORY_PATH = config_toml["savit"]["history_path"]
-
-    elif shell.find("bash") != -1:
-        config_toml_string = f"""[savit]
-history_path = "{home}/.bash_history"
-"""
-        with CONFIG_FILE.open("w") as f:
-            f.write(config_toml_string)
-
-        with CONFIG_FILE.open("rb") as f:
-            config_toml = tomllib.load(f)
-
-        HISTORY_PATH = config_toml["savit"]["history_path"]
-
-    elif shell.find("fish") != -1:
-        config_toml_string = f"""[savit]
-history_path = "{home}/.local/share/fish/fish_history"
-"""
-        with CONFIG_FILE.open("w") as f:
-            f.write(config_toml_string)
-
-        with CONFIG_FILE.open("rb") as f:
-            config_toml = tomllib.load(f)
-
-        HISTORY_PATH = config_toml["savit"]["history_path"]
+    if "zsh" in shell:
+        HISTORY_PATH = _get_shell_history(home, "/.zhistory")
+    elif "bash" in shell:
+        HISTORY_PATH = _get_shell_history(home, "/.bash_history")
+    elif "fish" in shell:
+        HISTORY_PATH = _get_shell_history(home, "/.local/share/fish/fish_history")
 
     print(HISTORY_PATH)
 
-    if confirmation_prompt := typer.confirm(
-        "is this your history file?", default=True
-    ):
+    if confirmation_prompt := typer.confirm("is this your history file?", default=True):
+        config_toml_string = f"""[savit]
+history_path = "{HISTORY_PATH}"
+"""
+        with CONFIG_FILE.open("w") as f:
+            f.write(config_toml_string)
+
         print("Config file saved")
 
     else:
-        HISTORY_PATH = Path(input("Enter the path of your history file: "))
+        HISTORY_PATH = input("Enter the path of your history file: ")
         config_toml_string = f"""[savit]
-history_path = {HISTORY_PATH}"""
+history_path = "{HISTORY_PATH}"
+"""
         with CONFIG_FILE.open("w") as f:
             f.write(config_toml_string)
 
@@ -80,3 +61,6 @@ history_path = {HISTORY_PATH}"""
             f.write(f'output_folder = "{output_folder}"\n')
         else:
             f.write('output_folder = "./"')
+
+    print(f"Options saved on {CONFIG_FILE}")
+    print("You can edit the config file later if you want to change the options")
