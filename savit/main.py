bc65bff9b07c3a723ac4bd6a62ea28d96701547f
ivansantiagojr <ivansantiago.junior@gmail.com>
2023-04-10 02:04:21 -0300

    style: run blue and isort

--- savit/main.py
+++ savit/main.py
@@ -1,18 +1,19 @@
-from pathlib import Path
 import tomllib
+from pathlib import Path
 from typing import Optional
 
 import typer
 
 from savit.config import CONFIG_FILE, set_env
 
-app = typer.Typer(help="Helping you to write docs by saving your commands")
+app = typer.Typer(help='Helping you to write docs by saving your commands')
 
 
 @app.command()
 def config(
     open_file: bool = typer.Option(
-        False, "--open-file", help="Opens the config file")
+        False, '--open-file', help='Opens the config file'
+    )
 ) -> None:
     """
     Saves your configurations to ~/.config/savit/config.toml
@@ -21,7 +22,7 @@ def config(
         typer.launch(str(CONFIG_FILE), locate=True)
         raise typer.Exit()
     if CONFIG_FILE.exists():
-        print("Config file already exists")
+        print('Config file already exists')
         raise typer.Exit()
 
     set_env()
@@ -36,8 +37,8 @@ def start() -> None:
     if not CONFIG_FILE.exists():
         set_env()
 
-    saving = typer.style("saving", fg=typer.colors.GREEN)
-    savit_stop = typer.style("savit stop", fg=typer.colors.BLUE)
+    saving = typer.style('saving', fg=typer.colors.GREEN)
+    savit_stop = typer.style('savit stop', fg=typer.colors.BLUE)
     typer.echo(
         f"""
                Started {saving}!
@@ -50,44 +51,46 @@ def start() -> None:
 @app.command()
 def stop(
     txt: bool = typer.Option(
-        False, "--txt", help="Saves your commands to a .txt file"),
+        False, '--txt', help='Saves your commands to a .txt file'
+    ),
     md: bool = typer.Option(
-        False, "--md", help="Saves your commands to a .md file"),
+        False, '--md', help='Saves your commands to a .md file'
+    ),
     file: Optional[Path] = typer.Option(
-        None, help="File (may include path) to save your commands"
+        None, help='File (may include path) to save your commands'
     ),
 ) -> None:
     """
     Stop saving your commands and writes them to a file
     """
     try:
-        with CONFIG_FILE.open("rb") as f:
+        with CONFIG_FILE.open('rb') as f:
             config_toml = tomllib.load(f)
-            hist_path = config_toml["savit"]["history_path"]
+            hist_path = config_toml['savit']['history_path']
 
-        with open(hist_path, "rb") as hist:
-            history_shell = hist.readlines()
-            hist_list = []
-            for i in range(len(history_shell)):
-                hist_list.append(history_shell[i].decode('utf-8'))
+        with open(hist_path, 'rb') as hist:
+            hist_list = [
+                command.decode('utf-8') for command in hist.readlines()
+            ]
 
-            print(hist_list)
-            start_index = len(hist_list) - 1 - \
-                hist_list[::-1].index("savit start\n")
-            saved_commands = hist_list[start_index + 1: -1]
+            start_index = (
+                len(hist_list) - 1 - hist_list[::-1].index('savit start\n')
+            )
+            saved_commands = hist_list[start_index + 1 : -1]
 
-        output_format = config_toml["savit"]["output_format"]
+        output_format = config_toml['savit']['output_format']
         if file is None:
             if txt:
-                output_format = "txt"
+                output_format = 'txt'
             elif md:
-                output_format = "md"
+                output_format = 'md'
             else:
-                output_format = config_toml["savit"]["output_format"]
+                output_format = config_toml['savit']['output_format']
 
-            output_folder = config_toml["savit"]["output_folder"]
-            output_file = str(Path(output_folder) /
-                              f"commands.{output_format}")
+            output_folder = config_toml['savit']['output_folder']
+            output_file = str(
+                Path(output_folder) / f'commands.{output_format}'
+            )
         elif txt or md:
             typer.secho(
                 "You can't use --txt or --md with --file. Use only one of them",
@@ -97,23 +100,23 @@ def stop(
             raise typer.Exit()
 
         elif not file.is_absolute():
-            output_folder = config_toml["savit"]["output_folder"]
+            output_folder = config_toml['savit']['output_folder']
             output_file = str(Path(output_folder) / file)
 
         else:
             output_file = str(file)
 
-        with open(output_file, "w") as commands:
+        with open(output_file, 'w') as commands:
             for command in saved_commands:
-                if output_format == "txt" or output_file.endswith(".txt"):
-                    commands.write(f"{command}")
-                elif output_format == "md" or output_file.endswith(".md"):
-                    commands.write(f"```console\n{command}```\n\n")
-            print("Commands saved")
+                if output_format == 'txt' or output_file.endswith('.txt'):
+                    commands.write(f'{command}')
+                elif output_format == 'md' or output_file.endswith('.md'):
+                    commands.write(f'```console\n{command}```\n\n')
+            print('Commands saved')
     except FileNotFoundError as e:
-        typer.echo(f"File not found: {e}", err=True)
+        typer.echo(f'File not found: {e}', err=True)
     except Exception as e:
-        typer.echo(f"Error: {e}", err=True)
+        typer.echo(f'Error: {e}', err=True)
 
 
 @app.callback()
@@ -124,5 +127,5 @@ def main() -> None:
     ...
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app()
--- tests/test_main.py
+++ tests/test_main.py
@@ -6,80 +6,85 @@ runner = CliRunner()
 
 
 def test_main_help():
-    result = runner.invoke(app, ["--help"])
+    result = runner.invoke(app, ['--help'])
     assert result.exit_code == 0
-    assert "Helping you to write docs by saving your commands" in result.output
+    assert 'Helping you to write docs by saving your commands' in result.output
 
 
 def test_start_help():
-    result = runner.invoke(app, ["start", "--help"])
+    result = runner.invoke(app, ['start', '--help'])
     assert result.exit_code == 0
-    assert "Start saving your commands" in result.output
+    assert 'Start saving your commands' in result.output
 
 
 def test_start_no_config():
-    result = runner.invoke(app, ["start"], input="y\nmd\n\n")
+    result = runner.invoke(app, ['start'], input='y\nmd\n\n')
     assert result.exit_code == 0
-    assert "Started saving" in result.output
+    assert 'Started saving' in result.output
 
 
 def test_start_with_config():
-    result = runner.invoke(app, ["start"])
+    result = runner.invoke(app, ['start'])
     assert result.exit_code == 0
-    assert "Started saving" in result.output
+    assert 'Started saving' in result.output
 
 
 def test_config_help():
-    result = runner.invoke(app, ["config", "--help"])
+    result = runner.invoke(app, ['config', '--help'])
     assert result.exit_code == 0
-    assert "Saves your configurations to ~/.config/savit/config.toml" in result.output
+    assert (
+        'Saves your configurations to ~/.config/savit/config.toml'
+        in result.output
+    )
 
 
 def test_config():
-    result = runner.invoke(app, ["config"], input="y\nmd\n\n")
+    result = runner.invoke(app, ['config'], input='y\nmd\n\n')
     assert result.exit_code == 0
     assert (
-        "You can edit the config file later if you want to change the options"
-        or "Config file already exists" in result.output
+        'You can edit the config file later if you want to change the options'
+        or 'Config file already exists' in result.output
     )
 
 
 def test_config_file_exists():
-    result = runner.invoke(app, ["config"])
+    result = runner.invoke(app, ['config'])
     assert result.exit_code == 0
-    assert "Config file already exists" in result.output
+    assert 'Config file already exists' in result.output
 
 
 def test_config_open_file():
-    result = runner.invoke(app, ["config", "--open-file"])
+    result = runner.invoke(app, ['config', '--open-file'])
     assert result.exit_code == 0
 
 
 def test_stop_help():
-    result = runner.invoke(app, ["stop", "--help"])
+    result = runner.invoke(app, ['stop', '--help'])
     assert result.exit_code == 0
-    assert "Stop saving your commands and writes them to a file" in result.output
+    assert (
+        'Stop saving your commands and writes them to a file' in result.output
+    )
 
 
 def test_stop():
-    result = runner.invoke(app, ["stop"])
+    result = runner.invoke(app, ['stop'])
     assert result.exit_code == 0
-    assert "Commands saved" in result.output
+    assert 'Commands saved' in result.output
 
 
 def test_stop_txt():
-    result = runner.invoke(app, ["stop", "--txt"])
+    result = runner.invoke(app, ['stop', '--txt'])
     assert result.exit_code == 0
-    assert "Commands saved" in result.output
+    assert 'Commands saved' in result.output
 
 
 def test_stop_md():
-    result = runner.invoke(app, ["stop", "--md"])
+    result = runner.invoke(app, ['stop', '--md'])
     assert result.exit_code == 0
-    assert "Commands saved" in result.output
+    assert 'Commands saved' in result.output
 
 
 def test_stop_file():
-    result = runner.invoke(app, ["stop", "--file", "test.txt"])
+    result = runner.invoke(app, ['stop', '--file', 'test.txt'])
     assert result.exit_code == 0
-    assert "Commands saved" in result.output
+    assert 'Commands saved' in result.output
